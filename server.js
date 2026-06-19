99f906852b72606b349fe76c3f45b7fc998907f1
Nova-primo <kyo24suke11@gmail.com>
2026-06-19 10:39:35 +0900

    add strength rule

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const WebSocket = require("ws");
 
 const app = express();
-
 app.use(express.static("public"));
 
 const PORT = process.env.PORT || 10000;
@@ -16,42 +15,67 @@ const wss = new WebSocket.Server({ server });
 let clients = [];
 let turn = 0;
 
+const strength = {
+    "3":1,"4":2,"5":3,"6":4,"7":5,"8":6,
+    "9":7,"10":8,"J":9,"Q":10,"K":11,"A":12,"2":13,
+    "JOKER":14
+};
+
+let currentStrength = 0;
+
 wss.on("connection", (ws) => {
     console.log("接続された");
 
     clients.push(ws);
 
     ws.on("message", (msg) => {
-    const text = msg.toString();
-    console.log("受信:", text);
+        const text = msg.toString();
+        console.log("受信:", text);
 
-    if (clients[turn] === ws) {
+        if (clients[turn] === ws) {
 
-        if (text === "パス") {
+            if (text === "パス") {
 
-            clients.forEach(c => {
-                c.send(JSON.stringify({
-                    type: "pass"
-                }));
-            });
+                clients.forEach(c => {
+                    c.send(JSON.stringify({
+                        type: "pass"
+                    }));
+                });
+
+            } else {
+
+                let rank;
+                if (text === "JOKER") {
+                    rank = "JOKER";
+                } else {
+                    rank = text.slice(1);
+                }
+
+                const power = strength[rank];
+
+                if (power > currentStrength) {
+
+                    currentStrength = power;
+
+                    clients.forEach(c => {
+                        c.send(JSON.stringify({
+                            type: "play",
+                            card: text
+                        }));
+                    });
+
+                } else {
+                    console.log("弱いカードは出せない！");
+                    return;
+                }
+            }
+
+            turn = (turn + 1) % clients.length;
 
         } else {
-
-            clients.forEach(c => {
-                c.send(JSON.stringify({
-                    type: "play",
-                    card: text
-                }));
-            });
-
+            console.log("今はあなたのターンじゃない");
         }
-
-        turn = (turn + 1) % clients.length;
-
-    } else {
-        console.log("今はあなたのターンじゃない");
-    }
-});
+    });
 
     ws.on("close", () => {
         clients = clients.filter(c => c !== ws);
