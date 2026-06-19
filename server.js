c8752696538f597812e427a557ca8476ccf36e60
Nova-primo <kyo24suke11@gmail.com>
2026-06-19 10:09:16 +0900

    fix script tag

--- server.js
+++ server.js
@@ -24,13 +24,17 @@ wss.on("connection", (ws) => {
     ws.on("message", (msg) => {
         console.log("受信:", msg.toString());
 
-         if (clients[turn] === ws) {
+        if (clients[turn] === ws) {
 
-                  clients.forEach(c => {
-                c.send(msg.toString());
+             clients.forEach(c => {
+                c.send(JSON.stringify({
+                    type: "play",
+                    card: msg.toString()
+                }));
             });
 
-               turn = (turn + 1) % clients.length;
+            turn = (turn + 1) % clients.length;
+
         } else {
             console.log("今はあなたのターンじゃない");
         }
@@ -40,7 +44,7 @@ wss.on("connection", (ws) => {
         clients = clients.filter(c => c !== ws);
         console.log("切断された");
 
-         if (turn >= clients.length) {
+        if (turn >= clients.length) {
             turn = 0;
         }
     });
